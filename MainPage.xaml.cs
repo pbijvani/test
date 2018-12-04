56cbc03936240dbe3c4d9029d810e927d8ec36eb
pbijvani <pbijvani@gmail.com>
2018-12-04 21:52:15 +0000

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public void OnOptionsDrawerOpen(object sender, EventArgs e)
+        {
+            //optionsDrawer.IsOpen = !optionsDrawer.IsOpen;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            List<int> list = new List<int>() { 1, 2, 3 };
+            listView.ItemsSource = list;
+
+        }
+
+    }
+}
